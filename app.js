64e7c4ec669f589206a03a691f3f3872b8639f8e
rishu488 <rishu488@gmail.com>
2020-08-13 22:05:53 +0530

    "Aws Cognito authentication in node js"

---
+++ app.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var routes = require('./routes');
+var app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json())
+// var routes = require('./routes');
+app.use('/', routes);
+module.exports = app;
---
+++ routes.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+var authController = require('./Controllers/AuthController');
+router.post('/auth/register', authController.register);
+router.post('/auth/login', authController.login);
+router.post('/auth/validate', authController.validate_token);
+// module.exports = router;
+var authMiddleware = require('./MIddleware/AuthMiddleware');
+router.get('/hello', authMiddleware.Validate);
+module.exports = router;
